e5b2b33e1264c279024bc8057c8ed78ffc0922c2
André Lamego <andreolamego@gmail.com>
2025-02-19 20:55:06 -0300

    IDE changed

--- src/controller/Exercicios.java
+++ src/controller/Exercicios.java
@@ -1,38 +1,41 @@
 package controller;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Exercicios {
-	public Exercicios() {
-		 super();
+    public Exercicios() {
+		super();
 	}
 	 
 	public void PreencheVetor(){
-		 int[] vet = new int[1000000];
+		int[] vet = new int[1000000];
 		 
-		 double tempoInicial = System.nanoTime();
+		double tempoInicial = System.nanoTime();
 		 
-		 for(int i : vet) {
-			 vet[i] = 0;
-		 }
+		for(int i : vet) {
+			vet[i] = 0;
+		}
 		 
-		 double tempoFinal = System.nanoTime();
+		double tempoFinal = System.nanoTime();
 		 
-		 double tempoTotal = tempoFinal - tempoInicial;
-		 tempoTotal = tempoTotal / Math.pow(10, 9);
-		 System.out.println("Tempo total: "+tempoTotal+"s");
+		double tempoTotal = tempoFinal - tempoInicial;
+		tempoTotal = tempoTotal / Math.pow(10, 9);
+		System.out.println("Tempo total: "+tempoTotal+"s");
 	 }
 	
-	public void SeparaFrase(Scanner scan) {
-		
-		System.out.println("Insira a frase:");
-		String frase = scan.nextLine();
-		
+	public void SeparaFrase() {
+		//System.out.println("Insira a frase:");
+		//String frase = scan.nextLine();
+		String frase = JOptionPane.showInputDialog("");
 		String[] vetFrase = frase.split(";");
 		
 		for (String x : vetFrase) {
-			System.out.println(x);a
+			System.out.println(x);
 		}
 		System.out.println("\nQntd. Palavras: " + vetFrase.length);
+	}
+	
+	public void Exercicio3() {
+		
 	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,39 +1,35 @@
 package view;
 
-import java.util.Scanner;
-
-import controller.*;
+import javax.swing.JOptionPane;
+import controller.Exercicios;
 
 public class Main {
-	public static void main(String[] args) {
-		Exercicios ex = new Exercicios();
-		
-		Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Exercicios ex = new Exercicios();
+        
 
-		int selecao = 0;
+        int selecao = 0;
 		
 		while (selecao != 9) {
 			
-			System.out.println("\nEscolha um exercício: \n1 - Ex1\n2 - Ex2\n3 - Ex3\n9 - Finaliza");
-			selecao = scan.nextInt();
+			selecao = Integer.parseInt(JOptionPane.showInputDialog("\nEscolha um exercício: \n1 - Ex1\n2 - Ex2\n3 - Ex3\n9 - Finaliza"));
+			//System.out.println("\nEscolha um exercício: \n1 - Ex1\n2 - Ex2\n3 - Ex3\n9 - Finaliza");
+			//selecao = scan.nextInt();
 			
 			switch (selecao) {
 				case 1:
 					ex.PreencheVetor();
 					break;
 				case 2:
-					ex.SeparaFrase(scan);
+					ex.SeparaFrase();
 					break;
 				case 3:
-				
+					ex.Exercicio3();
 					break;
 				case 9:
 					System.out.println("Finalizado");
 					break;
 			}
 		}
-		scan.close();
-		
-		
-	}
+    }
 }
