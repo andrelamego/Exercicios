c284f70bd3364f3a0a0ce9ee2bc14d5bcef8f870
André Lamego <andreolamego@gmail.com>
2025-02-19 21:27:08 -0300

    Added Ex3

--- src/controller/Exercicios.java
+++ src/controller/Exercicios.java
@@ -26,7 +26,7 @@ public class Exercicios {
 	public void SeparaFrase() {
 		//System.out.println("Insira a frase:");
 		//String frase = scan.nextLine();
-		String frase = JOptionPane.showInputDialog("");
+		String frase = JOptionPane.showInputDialog("Insira a frase:");
 		String[] vetFrase = frase.split(";");
 		
 		for (String x : vetFrase) {
@@ -36,6 +36,38 @@ public class Exercicios {
 	}
 	
 	public void Exercicio3() {
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Insira o tamanho do vetor:"));
 		
+		while (i > 100) {
+			System.out.println("O vetor deve ser menor ou igual a 100");
+			
+			i = Integer.parseInt(JOptionPane.showInputDialog("Insira o tamanho do vetor:"));
+		}		
+		int[] vet = new int[i];
+
+
+
+		vet = carregarVet(vet);
+
+		verifVet(vet);
+	}
+
+	private int[] carregarVet(int[] vet){
+		for (int i = 0; i < vet.length; i++) {
+			vet[i] = Integer.parseInt(JOptionPane.showInputDialog("vet["+i+"]:"));
+		}
+
+		return vet;
+	}
+
+	private void verifVet(int[] vet){
+		for (int i : vet) {
+			if(i % 2 == 1){
+				System.out.println(i+" <--- Impar");
+			}
+			else if(i % 10 == 0) {
+				System.out.println(i+" <--- Par & Multiplo de 10");
+			}
+		}
 	}
 }
